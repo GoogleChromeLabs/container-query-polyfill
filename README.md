7f068d010c3555128e2539c3e0df07c69aa7dbeb
Surma <surma@surma.dev>
2021-11-10 16:40:10 +0000

    First mini test is working

---
+++ README.md
@@ -0,0 +1,5 @@
+## Limitations
+
+- Container Queries will not work when nested inside a Media Query. For now, only top-level CQs
+- Container queries can only be specified using pixels
+- Just... don’t do weird interspersed comments, okay?
--- cqfill.ts
+++ cqfill.ts
@@ -1,29 +1,36 @@
 interface ContainerQueryDescriptor {
-  name: string;
+  name?: string;
   breakPoint: BreakPoint;
   className: string;
+  rules: Rule[];
 }
 
-export enum Measurement {
+function uid(): string {
+  return Array.from({ length: 16 }, () =>
+    Math.floor(Math.random() * 256).toString(16)
+  ).join("");
+}
+
+enum Measurement {
   MinWidth,
   MaxWidth,
   MinHeight,
   MaxHeight,
 }
 
-interface BreakPoint {
-  measurement: Measurement;
-  threshold: number;
-}
-
-type Comparator = (v: ResizeObserverSize, threshold: number) => boolean;
 // TODO: Fix for logical props
+type Comparator = (v: ResizeObserverSize, threshold: number) => boolean;
 const comparators: Map<Measurement, Comparator> = new Map([
   [Measurement.MaxHeight, (v, t) => v.blockSize <= t],
   [Measurement.MinHeight, (v, t) => v.blockSize >= t],
   [Measurement.MaxWidth, (v, t) => v.inlineSize <= t],
   [Measurement.MinWidth, (v, t) => v.inlineSize >= t],
 ]);
+
+interface BreakPoint {
+  measurement: Measurement;
+  threshold: number;
+}
 
 function isQueryFullfilled(
   breakpoint: BreakPoint,
@@ -37,63 +44,222 @@ function isQueryFullfilled(
   );
 }
 
-const queries: WeakMap<Element, ContainerQueryDescriptor[]> = new Map();
-const globalObserver = new ResizeObserver((entries) => {
+// function findClosestContainer(el: Element, name?: string): Element | undefined {
+//   // Start at parent, and walk the tree upwards.
+//   while(el) {
+//     el = el.parentElement;
+//     // If ascendant is not a container, keep walking
+//     if(!containers.has(el)) continue;
+//     // Ascendant is a container!
+//     // If no name was given, return the first container we can find.
+//     if(!name) return el;
+//     // Otherwise, check it’s the container with the right name.
+//     const containerNames = containers.get(el)!
+//     if(containerNames.includes(name)) return el;
+//   }
+//   // If we reach the root, no container was found.
+//   return undefined;
+// }
+
+function isAncestor(potentialAncestor: Element, descendant: Element): boolean {
+  while (descendant) {
+    // We explicitly start at the parent of `descendant`.
+    descendant = descendant.parentElement;
+    if (descendant === potentialAncestor) return true;
+  }
+  return false;
+}
+
+const containers: WeakMap<Element, string[]> = new WeakMap();
+function registerContainer(el: Element, name: string) {
+  containerRO.observe(el);
+  if (!containers.has(el)) {
+    containers.set(el, []);
+  }
+  containers.get(el)!.push(name);
+}
+const queries: Array<ContainerQueryDescriptor> = [];
+function registerContainerQuery(cqd: ContainerQueryDescriptor) {
+  queries.push(cqd);
+}
+const containerRO = new ResizeObserver((entries) => {
   for (const entry of entries) {
-    const queryList = queries.get(entry.target);
-    if (!queryList) continue;
-    for (const query of queryList) {
-      entry.target.classList.toggle(
-        query.className,
-        isQueryFullfilled(query.breakPoint, entry)
-      );
+    const container = entry.target;
+    for (const query of queries) {
+      for (const { selector } of query.rules) {
+        const els = document.querySelectorAll(selector);
+        for (const el of els) {
+          if (!isAncestor(container, el)) continue;
+          // *If* the container query had a name, check that we have the container
+          // with that name.
+          if (query.name) {
+            const containerNames = containers.get(container)!;
+            if (!containerNames.includes(query.name)) continue;
+          }
+          container.classList.toggle(
+            query.className,
+            isQueryFullfilled(query.breakPoint, entry)
+          );
+        }
+      }
     }
   }
 });
 
-function uid(): string {
-  return Array.from({ length: 16 }, () =>
-    Math.floor(Math.random() * 256).toString(16)
-  ).join("");
-}
+// function registerContainerQuery(
+//   cq: ContainerQueryDescriptor,
+//   rules: Rule[],
+//   {name = ""} = {}
+// ) {
+//   if (!queries.has(el)) {
+//     queries.set(el, []);
+//   }
+//   queries.get(el)!.push({
+//     className,
+//     name,
+//     breakPoint,
+//   });
+//   containerRO.observe(el);
+// }
 
-export function containerQuery(
-  el: Element,
-  breakPoint: BreakPoint,
-  className: string,
-  name: string = uid()
-) {
-  if (!queries.has(el)) {
-    queries.set(el, []);
-  }
-  queries.get(el)!.push({
-    className,
-    name,
-    breakPoint,
-  });
-  globalObserver.observe(el);
+interface WatchedSelector {
+  selector: string;
+  name: string;
 }
+const watchedContainerSelectors: WatchedSelector[] = [];
+const containerMO = new MutationObserver((entries) => {
+  for (const entry of entries) {
+    for (const node of entry.removedNodes) {
+      if (!(node instanceof HTMLElement)) continue;
+      containerRO.unobserve(node);
+    }
 
-const cqMatcher = /@container/g;
-export function parseStyleSheet(sheetSrc: string): string {
-  while (true) {
-    const match = cqMatcher.exec(sheetSrc);
-    if (!match) break;
-    const { query, substyles, startIndex, endIndex } = parseContainerQuery({
-      sheetSrc,
-      index: match.index,
-    });
-    sheetSrc =
-      sheetSrc.slice(0, startIndex) +
-      "/* INSERT CQ */" +
-      sheetSrc.slice(endIndex);
+    for (const node of entry.addedNodes) {
+      if (!(node instanceof HTMLElement)) continue;
+      for (const watchedContainerSelector of watchedContainerSelectors) {
+        if (node.matches(watchedContainerSelector.selector)) {
+          registerContainer(node, watchedContainerSelector.name);
+        }
+      }
+    }
   }
-  return sheetSrc;
-}
+});
+containerMO.observe(document.documentElement, {
+  childList: true,
+  subtree: true,
+});
 
 interface AdhocParser {
   sheetSrc: string;
   index: number;
+}
+
+export function parseStyleSheet(sheetSrc: string): string {
+  const p: AdhocParser = {
+    sheetSrc,
+    index: 0,
+  };
+
+  while (true) {
+    eatWhitespace(p);
+    if (p.index >= p.sheetSrc.length) break;
+    const nextIdent = peekIdentifier(p);
+    if (nextIdent.startsWith("/*")) {
+      eatComment(p);
+    } else if (nextIdent === "@container") {
+      const { query, startIndex, endIndex } = parseContainerQuery(p);
+      const replacement = stringifyContainerQuery(query);
+      replacePart(startIndex, endIndex, replacement, p);
+      registerContainerQuery(query);
+    } else {
+      const rule = parseRule(p);
+      if (rule.block.contents.includes("container-")) {
+        const name =
+          /container-name: ([^;]+);/.exec(rule.block.contents)?.[1] ?? uid();
+        const newBlock = rule.block.contents.replace(
+          "container-type",
+          "containment"
+        );
+        replacePart(rule.block.startIndex, rule.block.endIndex, newBlock, p);
+        watchedContainerSelectors.push({
+          name,
+          selector: rule.selector,
+        });
+        for (const el of document.querySelectorAll(rule.selector)) {
+          registerContainer(el, name);
+        }
+      }
+    }
+  }
+  return p.sheetSrc;
+}
+
+function replacePart(
+  start: number,
+  end: number,
+  replacement: string,
+  p: AdhocParser
+) {
+  p.sheetSrc = p.sheetSrc.slice(0, start) + replacement + p.sheetSrc.slice(end);
+  // If we are pointing past the end of the affected section, we need to recalculate
+  // the string pointer. Pointing to something inside the section that’s being replaced
+  // is undefined behavior. Sue me.
+  if (p.index >= end) {
+    const delta = p.index - end;
+    p.index = start + replacement.length + delta;
+  }
+}
+
+function eatComment(p: AdhocParser) {
+  assertString(p, "/*");
+  eatUntil("*/", p);
+}
+
+function eatUntil(s: string, p: AdhocParser): string {
+  const startIndex = p.index;
+  while (!lookAhead(s, p)) {
+    p.index++;
+  }
+  return p.sheetSrc.slice(startIndex, p.index);
+}
+
+function lookAhead(s: string, p: AdhocParser): boolean {
+  return p.sheetSrc.substr(p.index, s.length) == s;
+}
+
+interface Rule {
+  selector: string;
+  block: Block;
+  startIndex: number;
+  endIndex: number;
+}
+
+interface Block {
+  contents: string;
+  startIndex: number;
+  endIndex: number;
+}
+
+function parseRule(p: AdhocParser): Rule {
+  const startIndex = p.index;
+  const selector = eatUntil("{", p);
+  const block = eatBlock(p);
+  const endIndex = p.index;
+  return {
+    selector,
+    block,
+    startIndex,
+    endIndex,
+  };
+}
+
+function peekIdentifier(p: AdhocParser) {
+  identMatcher.lastIndex = p.index;
+  const match = identMatcher.exec(p.sheetSrc);
+  if (!match) {
+    return "";
+  }
+  return match[0];
 }
 
 function assertString(p: AdhocParser, s: string) {
@@ -117,7 +283,7 @@ function peek(p: AdhocParser): string {
   return p.sheetSrc[p.index];
 }
 
-const identMatcher = /[a-zA-Z_-]+/g;
+const identMatcher = /[\w\@_-]+/g;
 function parseIdentifier(p: AdhocParser): string {
   identMatcher.lastIndex = p.index;
   const match = identMatcher.exec(p.sheetSrc);
@@ -162,26 +328,25 @@ function parseThreshold(p: AdhocParser): number {
   return value;
 }
 
-function skipBlock(p: AdhocParser): string {
+function eatBlock(p: AdhocParser): Block {
+  const startIndex = p.index;
   assertString(p, "{");
-  const start = p.index;
   let level = 1;
   while (level != 0) {
-    p.index++;
     if (p.sheetSrc[p.index] === "{") {
       level++;
     } else if (p.sheetSrc[p.index] === "}") {
       level--;
     }
+    p.index++;
   }
-  const end = p.index - 1;
-  const block = p.sheetSrc.slice(start, end);
-  return block;
+  const endIndex = p.index;
+  const contents = p.sheetSrc.slice(startIndex, endIndex);
+  return { startIndex, endIndex, contents };
 }
 
 interface ParseResult {
   query: ContainerQueryDescriptor;
-  substyles: string;
   startIndex: number;
   endIndex: number;
 }
@@ -190,13 +355,11 @@ function parseContainerQuery(p: AdhocParser): ParseResult {
   const startIndex = p.index;
   assertString(p, "@container");
   eatWhitespace(p);
-  let name: string;
+  let name: string = "";
   if (peek(p) !== "(") {
     name = parseIdentifier(p);
-  } else {
-    name = uid();
+    eatWhitespace(p);
   }
-  eatWhitespace(p);
   assertString(p, "(");
   eatWhitespace(p);
   const measurement = parseMeasurementName(p);
@@ -207,10 +370,17 @@ function parseContainerQuery(p: AdhocParser): ParseResult {
   eatWhitespace(p);
   assertString(p, ")");
   eatWhitespace(p);
-  const substyles = skipBlock(p);
+  assertString(p, "{");
+  eatWhitespace(p);
+  const rules = [];
+  while (peek(p) !== "}") {
+    rules.push(parseRule(p));
+    eatWhitespace(p);
+  }
+  assertString(p, "}");
   const endIndex = p.index;
   eatWhitespace(p);
-  const className = uid();
+  const className = `cq_${uid()}`;
   return {
     query: {
       breakPoint: {
@@ -219,9 +389,17 @@ function parseContainerQuery(p: AdhocParser): ParseResult {
       },
       className,
       name,
+      rules,
     },
-    substyles,
     startIndex,
     endIndex,
   };
+}
+
+function stringifyContainerQuery(query: ContainerQueryDescriptor): string {
+  return query.rules
+    .map(
+      (rule) => `.${query.className} ${rule.selector} ${rule.block.contents}`
+    )
+    .join("\n");
 }
