d9a9c4604cec8addb94f42401ac416e0dc9845c8
Surma <surma@surma.dev>
2021-11-10 17:00:05 +0000

    Simple test harness

---
+++ tests/test-utils.js
@@ -0,0 +1,34 @@
+export function doubleRaf() {
+  return new Promise((resolve) => {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        resolve();
+      });
+    });
+  });
+}
+
+export function fail(msg) {
+  window.parent?.postMessage(msg, "*");
+}
+
+export function success() {
+  window.parent?.postMessage(true, "*");
+}
+
+export function assert(bool, msg) {
+  if (!bool) {
+    throw Error(msg);
+  }
+}
+
+export async function testSuite(name, cb) {
+  try {
+    await cb();
+  } catch (e) {
+    console.error(e);
+    fail(`${name}: ${e.msg}`);
+    return;
+  }
+  success();
+}
