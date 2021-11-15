93846fd5e37838f90d20b370e80f8c125a511001
Surma <surma@surma.dev>
2021-11-15 17:47:05 +0000

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
+# Container Query Polyfill
+
+A tiny polyfill for [CSS Container Queries][mdn], weighing about 1.6kB brotli’d. It transpiles CSS code on the client-side and implements Container Query functionality using [ResizeObserver] and [MutationObserver].
+
+## Usage
+
+Ideally, the polyfill is only loaded if the browser doesn’t support Container Queries natively. In a modern setup with a bundler that uses ES modules, the following snippet should work:
+
+```js
+const supportsContainerQueries = "container" in document.documentElement.style;
+if (!supportsContainerQueries) {
+  import("container-query-polyfill");
+}
+```
+
+If you are in a legacy setup (or just want to prototype quickly), there’s also an IIFE version that you can include using a `<script>` tag:
+
+```html
+<script src="https://unpkg.com/container-query-polyfill/cqfill.iffe.min.js"></script>
+```
+
+## Browser support
+
+The polyfull should work in all modern browsers. Chrome 88+, Firefox 78+ and Safari 14+.
+
 ## Limitations
 
-- Container Queries will not work when nested inside a Media Query. For now, only top-level CQs
-- Container queries can only be specified using pixels
-- Can’t handle cross-origin stylesheets.
-- Just... don’t do weird interspersed comments, okay?
+To keep the polyfill performant, small and maintainable, I have make certain tradeoffs with full feature parity of the Container Query spec. I have listed these tradeoffs below.
+
+(These decisions _can_ be revisited if they pose a significant hurdle and there is a good way to implement them. Please open an issue!)
+
+- Container Queries will not work when nested inside a Media Query. For now, the polyfill only supports top-level CQs.
+- Container query thresholds can only be specified using pixels.
+- Due to the nature of CORS, the polyfill only attempts to handle same-origin and inline stylesheets. Cross-origin stylesheets are ignored, regardless of CORS headers.
+- Don’t do weird interspersed comments, okay? Like `@container /* here’s a comment! */ (min-width: 1px) { ... }`. Just don’t.
+
+---
+
+License Apache-2.0
+
+[mdn]: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Container_Queries
+[resizeobserver]: https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
+[mutationobserver]: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
--- src/cqfill.ts
+++ src/cqfill.ts
@@ -13,39 +13,48 @@
 
 import { transpileStyleSheet } from "./engine.js";
 
-const sheetObserver = new MutationObserver((entries) => {
-  for (const entry of entries) {
-    for (const addedNode of entry.addedNodes) {
-      if (addedNode instanceof HTMLStyleElement) {
-        handleStyleTag(addedNode);
-      }
-      if (addedNode instanceof HTMLLinkElement) {
-        handleLinkedStylesheet(addedNode);
+function init() {
+  const sheetObserver = new MutationObserver((entries) => {
+    for (const entry of entries) {
+      for (const addedNode of entry.addedNodes) {
+        if (addedNode instanceof HTMLStyleElement) {
+          handleStyleTag(addedNode);
+        }
+        if (addedNode instanceof HTMLLinkElement) {
+          handleLinkedStylesheet(addedNode);
+        }
       }
     }
+  });
+  sheetObserver.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
+
+  function handleStyleTag(el: HTMLStyleElement) {
+    const newSrc = transpileStyleSheet(el.innerHTML);
+    el.innerHTML = newSrc;
   }
-});
-sheetObserver.observe(document.documentElement, {
-  childList: true,
-  subtree: true,
-});
 
-function handleStyleTag(el: HTMLStyleElement) {
-  const newSrc = transpileStyleSheet(el.innerHTML);
-  el.innerHTML = newSrc;
+  async function handleLinkedStylesheet(el: HTMLLinkElement) {
+    if (el.rel !== "stylesheet") return;
+    const srcUrl = new URL(el.href, import.meta.url);
+    if (srcUrl.origin !== location.origin) return;
+    const src = await fetch(srcUrl.toString()).then((r) => r.text());
+    const newSrc = transpileStyleSheet(src);
+    const blob = new Blob([newSrc], { type: "text/css" });
+    el.href = URL.createObjectURL(blob);
+  }
+
+  document.querySelectorAll("style").forEach((tag) => handleStyleTag(tag));
+  document
+    .querySelectorAll("link")
+    .forEach((tag) => handleLinkedStylesheet(tag));
 }
 
-async function handleLinkedStylesheet(el: HTMLLinkElement) {
-  if (el.rel !== "stylesheet") return;
-  const srcUrl = new URL(el.href, import.meta.url);
-  if (srcUrl.origin !== location.origin) return;
-  const src = await fetch(srcUrl.toString()).then((r) => r.text());
-  const newSrc = transpileStyleSheet(src);
-  const blob = new Blob([newSrc], { type: "text/css" });
-  el.href = URL.createObjectURL(blob);
+const supportsContainerQueries = "container" in document.documentElement.style;
+if (!supportsContainerQueries) {
+  init();
 }
-
-document.querySelectorAll("style").forEach((tag) => handleStyleTag(tag));
-document.querySelectorAll("link").forEach((tag) => handleLinkedStylesheet(tag));
 
 export { transpileStyleSheet };
