500f4ca1bcddd2d4f438e8be09537c38b5b7ed12
Gerald Monaco <gbmonaco@google.com>
2022-10-18 17:00:32 +0000

    Increase timeout multiplier

--- src/wpt.ts
+++ src/wpt.ts
@@ -33,7 +33,7 @@ export function initializeForWPT() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).testharness_properties = {
     output: true,
-    timeout_multiplier: 20,
+    timeout_multiplier: 100,
   };
 
   window.addEventListener('error', e => {
